e1f8db0aef90fe9fcfd5ebd0c6e3c92565631273
Shurav Nandy <nandyshurav@gmail.com>
2022-09-05 16:55:49 -0400

    more edits

--- main2.py
+++ main2.py
@@ -1,4 +1,6 @@
 import numpy
+import seaborn
+import matplotlib.pyplot as plt
 a = int(input("enter the number of values to insert: "))
 numarray = []
 for i in range(a):
@@ -19,3 +21,10 @@ e = numpy.random.choice([4, 1, 2], size = (3, 2))
 print(e)
 f = numpy.random.choice([5, 1, 4, 9], p = [0.2, 0, 0.5, .3], size = (4, 1, 5))
 print(f)
+print('permutation', numpy.random.permutation(f), 'original', f)
+numpy.random.shuffle(f)
+print('shuffle', f, 'shape', f.shape)
+seaborn.distplot([0, 1, 2, 3, 4, 5], hist = True)
+plt.show()
+seaborn.distplot(numpy.random.normal(size = (10, 2)), hist = False)
+plt.show()
